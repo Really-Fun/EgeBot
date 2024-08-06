17831de27224c3d78bfde99d8060677319270630
ReallyChubrik <ilyareztcov@gmail.com>
2024-08-06 12:24:38 +0300

    Add start test_handler

--- handlers/database_handlers.py
+++ handlers/database_handlers.py
@@ -15,7 +15,3 @@ def check_base(message: Message):
 async def write(message: Message):
     baza.write_to_database(message.from_user.id)
     await message.answer(commands['/start'], reply_markup=keyboard_start)
-
-@router.message(F.text.lower() == 'а')
-async def gaga(message: Message):
-    await message.answer(f'<a href="tg://user?id={1082861671}"><u><b>И</b></u></a>')
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,18 +1,26 @@
 from unittest.mock import AsyncMock
 import pytest
 
-from handlers.default_handlers import get_help
+from handlers.default_handlers import get_help, get_start
 from keyboards import keyboard_start
 from lexicon_ru.lexicon import commands
 
 
 @pytest.mark.asyncio
-async def test_start_handler():
+async def test_help_handler():
     message = AsyncMock()
-
     await get_help(message)
 
     message.answer.assert_called_with(commands["/help"], reply_markup=keyboard_start)
+
+
+@pytest.mark.asyncio
+async def test_start_handler():
+    message = AsyncMock()
+    message.from_user.id = "1111111"
+    await get_start(message)
+
+    message.answer.assert_called_with(commands["/start"], reply_markup=keyboard_start)
 
 
 if __name__ == "__main__":
